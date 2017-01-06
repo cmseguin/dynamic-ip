c41a647782328f3bd2191f4debe7fc8ebb7b49f0
Charles-Mathieu Seguin <charles.mathieu.seguin@gmail.com>
2017-01-05 21:57:52 -0500

    Fix an issue with creating a file for log

---
+++ README.md
@@ -0,0 +1,54 @@
+# Dynamic Ip
+
+### Description
+Just a simple script that pings a 3rd party to find out if your ip changed every
+x amount of time. If it changes, it will update specified domains and records.
+
+### Installation
+
+Create a file called config-local.js at the root of the project:
+```javascript
+module.exports = {
+    'debug': true,
+    'digitalOceanToken': 'xxx',
+    'domains': [
+        {
+            'domain': 'foo.com',
+            'records': ['bar', 'baz']
+        }
+    ]
+}
+```
+
+Then run:
+```bash
+npm install
+```
+
+### Hooking it up with systemd
+
+Install the dependencies:
+```bash
+npm install --production
+```
+
+Clone the project in your home dir:
+```bash
+cd ~
+git clone git@gitlab.com:cmseguin/dynamic-ip.git
+mv ./dynamic-ip ./.dynamic-ip
+```
+
+Create a file in /etc/systemd/system called dynamic-ip.service
+
+```
+[Unit]
+Description=A script to update one or more domains record to point to a non-static ip address
+After=network.target
+
+[Service]
+Type=fork
+WorkingDirectory=/home/{user}
+ExecStart=/usr/local/bin/node /home/{user}/.dynamic-ip/index.js
+Restart=on-abort
+```
--- services/logger.js
+++ services/logger.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 const winston = require('winston');
 const dateFormat = require('dateformat');
@@ -6,8 +7,8 @@ const config = require('./config');
 const transports = [];
 
 if (config.get('debug') === true) {
-    transports.push(new (winston.transports.Console)({
-        'formatter': function (options) {
+    transports.push(new winston.transports.Console({
+        'formatter': function formatter (options) {
             const now = new Date();
             const prefix = `[${dateFormat(now, 'yyyy-mm-dd HH:MM:ss')}]`;
             const message = options.message || '';
@@ -17,12 +18,21 @@ if (config.get('debug') === true) {
         }
     }));
 } else {
-    transports.push(new (winston.transports.File)({
-        'filename': path.join('../', config.get('logDestination'), 'stdout.log')
+    const logFile = 'stdout.log';
+    const logDir = path.resolve(__dirname, '../', config.get('logDestination'));
+    const logDest = path.join(logDir, logFile);
+
+    if (!fs.existsSync(logDir)) {
+        // Create the directory if it does not exist
+        fs.mkdirSync(logDir);
+    }
+
+    transports.push(new winston.transports.File({
+        'filename': logDest
     }));
 }
 
-const logger = new (winston.Logger)({
+const logger = new winston.Logger({
     transports
 });
 
